b0bd22270d5053243200ce1ee27387035f139d5b
Faysal Ahmed <techbanglamaxfs@gmail.com>
2025-12-29 01:18:26 +0600

    update 1.6

--- lib/app/assets_paths.dart
+++ lib/app/assets_paths.dart
@@ -1,4 +1,5 @@
 class AssetsPaths {
   static const String _baseImagePath = 'assets/images';
   static const String logoSvg = '$_baseImagePath/logo.svg';
+  static const String vanLogoSvg = '$_baseImagePath/logo_nav.svg';
 }
--- lib/features/home/presantation/screens/home_screen.dart
+++ lib/features/home/presantation/screens/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:crafty_bay/app/assets_paths.dart';
+import 'package:crafty_bay/features/home/presantation/widget/home_carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widget/circle_icon_button.dart';
+import '../widget/product_search_field.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +16,37 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            spacing: 16,
+            children: [
+
+              ProductSearchField(),
+              HomeCarouselSlider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Row(
+        spacing: 4,
+        children: [
+          SvgPicture.asset(AssetsPaths.vanLogoSvg),
+          Spacer(),
+          CircleIconButton(icon: Icons.person, onTap: () {}),
+          CircleIconButton(icon: Icons.call, onTap: () {}),
+          CircleIconButton(icon: Icons.notifications_active, onTap: () {}),
+        ],
+      ),
+    );
   }
 }
+
