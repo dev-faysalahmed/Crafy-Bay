34f0c23c8306631f9bca5514c5c02ba485679e6f
Faysal Ahmed <techbanglamaxfs@gmail.com>
2025-12-30 17:36:17 +0600

    update 1.13

--- lib/features/wish_list/presentation/screens/wish_list_screen.dart
+++ lib/features/wish_list/presentation/screens/wish_list_screen.dart
@@ -1,4 +1,6 @@
+import 'package:crafty_bay/features/common/presentation/providers/main_nav_container_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../../../common/presentation/widget/product_card.dart';
 
 class WishListScreen extends StatefulWidget {
@@ -11,18 +13,31 @@ class WishListScreen extends StatefulWidget {
 class _WishListScreenState extends State<WishListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Wish List')),
-      body: GridView.builder(
-        itemCount: 10,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 8,
-            crossAxisSpacing: 0
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        context.read<MainNavContainerProvider>().backToHome();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Row(
+          children: [
+            IconButton(onPressed: (){
+              context.read<MainNavContainerProvider>().backToHome();
+            }, icon: Icon(Icons.arrow_back_ios)),
+            Text('Wish List'),
+          ],
+        )),
+        body: GridView.builder(
+          itemCount: 10,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 0
+          ),
+          itemBuilder: (context, index) {
+            return FittedBox(child: ProductCard());
+          },
         ),
-        itemBuilder: (context, index) {
-          return FittedBox(child: ProductCard());
-        },
       ),
     );
   }
