b1fcf6882fb9f7fac1a1c53085ed9fcd93c260ad
Faysal Ahmed <techbanglamaxfs@gmail.com>
2026-01-03 23:47:34 +0600

    update 1.25

--- lib/features/home/presantation/providers/home_slider_provider.dart
+++ lib/features/home/presantation/providers/home_slider_provider.dart
@@ -2,5 +2,10 @@ import 'package:flutter/material.dart';
 
 class HomeSliderProvider extends ChangeNotifier{
 
+  bool _getHomeSliderInProgress = false;
+
+  bool get getHomeSliderInProgress => _getHomeSliderInProgress;
+
+
 
 }
